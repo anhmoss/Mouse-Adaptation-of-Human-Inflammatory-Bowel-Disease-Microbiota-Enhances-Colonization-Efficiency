c147b3bc82d0ad91268ebf703df246b8cf813a23
anhmoss <anh.moss@gmail.com>
2023-03-03 15:53:52 -0500

    Update getCorrelationCoefficient_withinGroup.R

--- R/getCorrelationCoefficient_withinGroup.R
+++ R/getCorrelationCoefficient_withinGroup.R
@@ -1,16 +1,18 @@
-## correlations for all possible sample pairs WITHIN Groups
-#input: list of group names, dataframe with counts and metadata, dataframe with counts only (lognormalized), correlation method
-#output: a list of lists of correlation coefficient for sample pairs across groups 
-
+## correlations for samples WITHIN Groups
+#input: list of group names, dataframe with counts and metadata, dataframe with counts only (lognormalized), correlation test, variableName is the column name of metadata, sampleColumnName is the column name of sample names
+#output: a list of group pair labels for each correlation,a list of lists of correlation coefficient for sample pairs across groups 
 
 getCorrelationCoefficient_withinGroup = function(groupList, tableWithMeta, tableCountsOnly, 
-                                             corrTestMethod){
+                                                 corrTestMethod, variableName, sampleColumnName){
   
   corr_rho_withinGroups_list = list()
   
   for(m in 1:length(groupList)){
     
-    samples_firstGroup = tableWithMeta$SampleID[tableWithMeta$FMTGroupFMTsourcegtRecipientbackground==groupList[m]]
+    samples.index = which(colnames(tableWithMeta)==sampleColumnName)
+    variable.index = which(colnames(tableWithMeta)==variableName)
+    
+    samples_firstGroup = tableWithMeta[,samples.index][tableWithMeta[,variable.index]==groupList[m]]
     df_firstGroup = tableCountsOnly[rownames(tableCountsOnly)%in%samples_firstGroup,]
     labels_firstGroup = groupList[m]
     corr_rho_perGroup = vector()
