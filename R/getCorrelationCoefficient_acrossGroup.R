5f0ec219dcfa10808faa10b6b5afeffa0faa622b
anhmoss <anh.moss@gmail.com>
2023-03-04 01:48:17 -0500

    Update getCorrelationCoefficient_acrossGroup.R

--- R/getCorrelationCoefficient_acrossGroup.R
+++ R/getCorrelationCoefficient_acrossGroup.R
@@ -1,10 +1,10 @@
 ## generates correlation coefficient for sample pairs ACROSS Groups
 #input: directory path for results, list of interest groups/labels, dataframe with counts and metadata, 
-## dataframe with counts only (lognormalized), correlation method, 
+## dataframe with counts only (lognormalized), correlation test, variableName is the column name of metadata, sampleColumnName is the column name of sample names
 #output: a list of group pair labels for each correlation,a list of lists of correlation coefficient for sample pairs across groups 
 
 getCorrelationCoefficient_acrossGroup = function(groupList, tableWithMeta, tableCountsOnly, 
-                                                 corrTestMethod){
+                                                 corrTestMethod, variableName, sampleColumnName){
   
   corr_acrossGroups_list = list()
   labels_groupPairs = list()
@@ -14,8 +14,11 @@ getCorrelationCoefficient_acrossGroup = function(groupList, tableWithMeta, table
     
     for(n in (m+1):length(groupList)){
       
-      samples_firstGroup = tableWithMeta$SampleID[tableWithMeta$FMTGroupFMTsourcegtRecipientbackground==groupList[m]]
-      samples_secondGroup = tableWithMeta$SampleID[tableWithMeta$FMTGroupFMTsourcegtRecipientbackground==groupList[n]]
+      samples.index = which(colnames(tableWithMeta)==sampleColumnName)
+      variable.index = which(colnames(tableWithMeta)==variableName)
+      
+      samples_firstGroup = tableWithMeta[,samples.index][tableWithMeta[,variable.index]==groupList[m]]
+      samples_secondGroup = tableWithMeta[,samples.index][tableWithMeta[,variable.index]==groupList[n]]
       
       df_firstGroup = tableCountsOnly[rownames(tableCountsOnly)%in%samples_firstGroup,]
       df_secondGroup = tableCountsOnly[rownames(tableCountsOnly)%in%samples_secondGroup,]
