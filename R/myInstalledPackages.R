ea622ea2a53d17e2aa853bb1cd0fcfe234f3732e
anhmoss <anh.moss@gmail.com>
2023-04-23 15:09:04 -0400

    Add files via upload

--- R/myInstalledPackages.R
+++ R/myInstalledPackages.R
@@ -1,5 +1,5 @@
-# Installing and loading packages for this analysis
+# Installing and loading packages for this study
 
-packagesToInstall = c("ggplot2", "vegan", "dplyr", "testthat")
+packagesToInstall = c("ggplot2", "vegan", "dplyr", "testthat", "stats","car")
 install.packages(packagesToInstall) 
 lapply(packagesToInstall, library, character.only=TRUE)
